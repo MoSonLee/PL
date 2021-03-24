e14d019c53a2a0f6face4ee9f3e56f0220a313aa
이승후 <leeseunghoo@iseunghuui-MacBookAir.local>
2021-03-24 17:20:51 +0900

    first commit

---
+++ hw1/hw1.ml
@@ -0,0 +1,19 @@
+type day = SUN | MON | TUE | WED | THU | FRI | SAT
+type grade = Aplus | A | Bplus | B | Cplus | C | Dplus | D | F
+    
+let student_number = -1;;
+let email = "";;
+let phone_number = "";;
+let favorite_day = FRI;;
+let expected_grade = Aplus;;
+let is_male = false;;
+let result = 0.0;;
+
+let student_number = 201613454;;
+let email = "ron222@naver.com";;
+let phone_number ="010-9953-6235";;
+let favorite_day = SAT;;
+let expected_grade = A;;
+let is_male = true;;
+let result = 10. /. 4.;;
+
